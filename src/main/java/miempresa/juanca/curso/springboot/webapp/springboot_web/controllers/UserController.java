5fc6f331749691a83f97f5b35f24162742f90862
juancadev_ <juancarlosulloav@gmail.com>
2024-09-05 23:20:50 -0400

    C

---
+++ src/main/java/miempresa/juanca/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,8 @@
+package miempresa.juanca.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+        
+}
