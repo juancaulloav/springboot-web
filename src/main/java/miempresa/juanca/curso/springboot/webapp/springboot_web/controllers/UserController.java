e1bcf6345beed5dddfd6e930534ed6ced8a09ec8
juancadev_ <juancarlosulloav@gmail.com>
2024-09-05 23:33:25 -0400

    Se agrega la vista profile.html
    utilizando thymeleaf creamos una primera vista

--- src/main/java/miempresa/juanca/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/miempresa/juanca/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -2,7 +2,6 @@ package miempresa.juanca.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
