1ee3c4a2bf367548cb81eac5b2316b70697128f9
juancadev_ <juancarlosulloav@gmail.com>
2024-09-05 23:23:44 -0400

    Creacion primer controlador, profile

--- src/main/java/miempresa/juanca/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/miempresa/juanca/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,8 +1,15 @@
 package miempresa.juanca.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
-        
+
+    @GetMapping("/profile")
+        public String profile() {
+                return "profile";
+        }
 }
